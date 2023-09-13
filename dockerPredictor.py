84426496c295544b41564d107169691eace07e56
João Pedro <pedrorochacmr2013@gmail.com>
2023-09-13 14:15:00 +0100

    dockerPredictor

--- dockerPredictor.py
+++ dockerPredictor.py
@@ -1,13 +1,14 @@
 import requests
 import json
-from typing import Dict
 
 def predict_with_docker(messages: list):
-    docker_server_url = "http://192.168.0.4:8080/predict"
+    docker_server_url = "http://192.168.0.7:8081/predict"
 
     try:
-        data = json.dumps(messages)
-        response = requests.post(docker_server_url, data=data)
+        data = json.dumps({"instances": messages})  # Envie a lista de mensagens dentro de "instances"
+        headers = {"Content-Type": "application/json"}  # Defina o cabeçalho para indicar que você está enviando JSON
+
+        response = requests.post(docker_server_url, data=data, headers=headers)  # Faça uma solicitação POST
 
         if response.status_code == 200:
             result = response.json()
@@ -27,4 +28,14 @@ def process_predictions(predictions: list):
             with open("ReceivedMessages/DangerousMessages.txt", "a") as file:
                 file.write(message)
 
+def main():
+    # Exemplo de lista de mensagens contendo apenas "ID" e "DLC"
+    messages = [
+        {"ID": 123, "DLC": 112233},
+        {"ID": 234, "DLC": 223344},
+    ]
 
+    predict_with_docker(messages)
+
+if __name__ == "__main__":
+    main()
